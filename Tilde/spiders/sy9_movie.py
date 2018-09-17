27b23d24c2357a810fdaf168f3f75ecf1af29238
Gwynn <popmusicbbq@126.com>
2018-09-17 22:58:16 +0800

    update

--- Tilde/spiders/sy9_movie.py
+++ Tilde/spiders/sy9_movie.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-import scrapy
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.http import Request
+from scrapy.spiders import CrawlSpider
 
 from Tilde.items import Sy9MovieItem, Sy9MovieItemDetail
 
@@ -9,21 +8,24 @@ from Tilde.items import Sy9MovieItem, Sy9MovieItemDetail
 class Sy9MovieSpider(CrawlSpider):
     name = 'sy9-movie'
     allowed_domains = ['sy9d.com']
+
     start_urls = [
         "http://www.sy9d.com/forum-179-1.html",
+        # "http://www.sy9d.com/forum.php?mod=forumdisplay&fid=179",
     ]
+    # rules = [
+    #     Rule(LinkExtractor(allow=r'/forum-179-\d+.html'), callback='parse_movie_list',
+    #          follow=True),
+    # ]
 
     custom_settings = {
         'ITEM_PIPELINES': {
             'demo.pipelines.Sy9Pipelines': 300,
+            'demo.pipelines.Sy9ImgPipelines': 2,
         }
     }
 
-    rules = (
-        Rule(LinkExtractor(allow=r'/forum-179-\d.html'), callback='parse_item', follow=True),
-    )
-
-    def parse_item(self, response):
+    def parse(self, response):
         tbody = response.xpath('//tbody[re:test(@id,"^normalthread_.*")]')
         for movie in tbody:
             title = movie.css("div.tl_ct > a::text").extract_first()
@@ -31,26 +33,43 @@ class Sy9MovieSpider(CrawlSpider):
             item = Sy9MovieItem(title=title, url=url)
             yield item
 
-        request = scrapy.Request(url=url, callback=self.parse_movie_detail)
-        request.meta['url'] = url
-        yield request
+            request = Request(url=url, callback=self.parse_movie_detail)
+            request.meta['url'] = url
+            yield request
+
+        # next_page_url = response.css("a.nxt::attr(href)").extract_first()
+        # if next_page_url is not None:
+        #     yield Request(next_page_url)
 
     def parse_movie_detail(self, response):
         domain = "http://www.sy9d.com/"
         url = response.meta['url']
-        imgUrl = response.css('ignore_js_op > img::attr(zoomfile)').extract()
-        for index in range(len(imgUrl)):
-            imgUrl[index] = domain + imgUrl[index]
+        imgUrlTmp = response.css('ignore_js_op > img::attr(zoomfile)').extract()
+        imgUrl = ""
+        description = ""
+        file_urls = []
+        for index in range(len(imgUrlTmp)):
+            if imgUrlTmp[index]:
+                if imgUrl:
+                    imgUrl = imgUrl + ";" + domain + imgUrlTmp[index]
+                else:
+                    imgUrl = domain + imgUrlTmp[index]
+                file_urls.append(domain + imgUrlTmp[index])
 
-        description = response.css('font::text').extract()
-        delList = []
-        for index in range(len(description)):
-            if description[index] == '\r\n' or (not description[index]):
-                delList.append(index)
+        descriptionTmp = response.css('font::text').extract()
+        for index in range(len(descriptionTmp)):
+            s = self.clean_spaces(descriptionTmp[index])
+            if s:
+                description = description + s;
 
-        for i in delList:
-            del description[i]
+        if imgUrl:
+            detailItem = Sy9MovieItemDetail(url=url, imgUrl=imgUrl, description=description, file_urls=file_urls)
+            yield detailItem
 
-        detailItem = Sy9MovieItemDetail(url=url, imgUrl=imgUrl, description=description)
-
-        yield detailItem
+    def clean_spaces(self, str):
+        str = str.replace('\r\n', '')
+        str = str.replace('\t', ' ')
+        str = str.replace('\f', ' ')
+        str = str.replace('\r', '')
+        str = str.replace('\n', '')
+        return str
